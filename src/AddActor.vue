a601f30b56277cc8980913774245e0ebf6fd7a2b
Luiss Migryk <luis.migryk@usach.cl>
2017-09-08 15:54:15 -0300

    + Added validation for model actor

--- src/AddActor.vue
+++ src/AddActor.vue
@@ -4,13 +4,15 @@
     
       <p>
         <label for="firstName">Nombre</label>
-        <input type="text" id="firstName" name="firstName" v-model="actor.firstName" onClick="this.select();">
+        <input type="text" id="firstName" name="firstName" required v-model="actor.firstName" onClick="this.select();" >
       </p>
       <p>
         <label for="lastName">Apellido</label>
-        <input type="text" id="lastName" name="lastName" v-model="actor.lastName" onClick="this.select();">
+        <input type="text" id="lastName" name="lastName" required v-model="actor.lastName" onClick="this.select();" >
       </p>
-      <p><button v-on:click="addActor()">Agregar Actor</button></p>
+      <p><button v-on:click="addActor()"  >Agregar Actor</button></p>
+      <p id="demo"></p>
+    
     
   </div>
 </template>
@@ -25,8 +27,27 @@
     },
     methods:{
       addActor:function(){
-        this.$http.post('http://localhost:8080/TallerSakilaTBD/actors', this.actor)
+        if (firstName.value == 0 && lastName.value == 0){
+          alert("Tus campos no fueron llenados");
+        }
+        else if (firstName.value == 0){
+          alert("No podemos ingresar a tu actor sin nombre");
+        }
+        else if (lastName.value == 0){
+          alert("No podemos ingresar a tu actor sin apellido");
+        }
+        else{
+            var txt;
+              if (confirm("Deseas agregar a " + firstName.value + "?" ) == true) {
+                txt = "usuario creado";
+                this.$http.post('http://localhost:8080/TallerSakilaTBD/actors', this.actor);
+              } 
+              else {
+                txt = "cancelaste la creacion de tu actor";
+              }
+              document.getElementById("demo").innerHTML = txt;
+          }
+        }
       }
     }
-  }
 </script>
